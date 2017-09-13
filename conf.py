677b35c139ec1fd5964d8e218698b38d260a02b6
josedom24 <josedom24@gmail.com>
2017-09-13 22:59:52 +0200

    readthedocs

--- conf.py
+++ conf.py
@@ -30,15 +30,18 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 import recommonmark
+from recommonmark.parser import CommonMarkParser
 from recommonmark.transform import AutoStructify
 
 # At the bottom of conf.py
 def setup(app):
     app.add_config_value('recommonmark_config', {
-            'url_resolver': lambda url: github_doc_root + url,
-            'auto_toc_tree_section': 'Contents',
-            }, True)
-    app.add_transform(AutoStructify)
+        'url_resolver': lambda url: github_doc_root + url,
+        'auto_toc_tree_section': 'Contents',
+        'enable_eval_rst': True,
+        'enable_auto_doc_ref': True,
+    }, True)
+app.add_transform(AutoStructify)
 
 
 extensions = [
